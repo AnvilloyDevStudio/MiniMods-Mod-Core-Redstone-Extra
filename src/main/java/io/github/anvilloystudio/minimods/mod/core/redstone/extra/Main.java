d27199273a1ee525ec2bedf31bd485dbb432fbc6
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2023-07-17 16:18:25 +0800

    Last update

--- src/main/java/io/github/anvilloystudio/minimods/mod/core/redstone/extra/Main.java
+++ src/main/java/io/github/anvilloystudio/minimods/mod/core/redstone/extra/Main.java
@@ -1,13 +1,15 @@
 package io.github.anvilloystudio.minimods.mod.core.redstone.extra;
 
 import io.github.anvilloystudio.minimods.mod.core.redstone.extra.items.Items;
+import io.github.anvilloystudio.minimods.mod.core.redstone.extra.tiles.Tiles;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("THIS JAR IS NOT PURPOSED TO BE EXECUTED DIRECTLY.");
 	}
 
-    public static void entry() {
+	public static void entry() {
 		Items.postInit();
-    }
+		Tiles.postInit();
+	}
 }
---
+++ src/main/java/io/github/anvilloystudio/minimods/mod/core/redstone/extra/tiles/Tiles.java
@@ -0,0 +1,7 @@
+package io.github.anvilloystudio.minimods.mod.core.redstone.extra.tiles;
+
+public class Tiles {
+	public static void postInit() {
+
+	}
+}
